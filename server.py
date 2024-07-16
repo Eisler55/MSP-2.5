0a88a4d509854575fc2532af27f28b176bada279
Eisler55 <eislerandres1529@gmail.com>
2024-07-16 20:43:28 +0000

    Mundo guardado 2024-07-16

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 # Versiones disponibles
-# forge, mohist, fabric, vanilla, paper
+# forge, mohist, fabric, vanilla, 3
+
 
 # Puedes instalar mohist después de instalar forge desde el menú de gestionar
 # Puedes instalar paper después de instalar vanilla desde el menú de gestionar
@@ -16,14 +17,6 @@
 # sa		      Sudamérica (São Paulo)
 # us		      Estados unidos (Ohio)
 # us-cal-1	      Estados unidos (California)
-
-
-
-
-
-
-
-
 
 
 
